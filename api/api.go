8d8d31d3b39e097d4f761683102398791ce950dc
Zen <zhangyiming748@gmail.com>
2021-03-17 16:05:08 +0800

    设置http状态码

---
+++ api/api.go
@@ -0,0 +1,19 @@
+package api
+
+const (
+	RESPONSE_CODE_SUCCESS     string = "1"
+	RESPONSE_CODE_FAIL        string = "0"
+	RESPONSE_STATUS_CANCEL    string = "W"
+	RESPONSE_STATUS_CANCEL_OK string = "R"
+	RESPONSE_STATUS_CANCEL_N  string = "N"
+)
+
+type ResponseStatus string
+type AppResponse struct {
+	ResCode       string         `json:"resCode"`
+	ResStatus     ResponseStatus `json:"resMessage"`
+	RetData       interface{}    `json:"retData"`
+	RequsetAction string         `json:"requsetAction"`
+	UserToken     string         `json:"userToken"`
+	ServerTime    string         `json:"serverTime"`
+}
