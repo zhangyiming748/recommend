d99150731d5ab3d0e7c756947738afc3415fd8b2
Zen <zhangyiming748@gmail.com>
2021-03-21 13:51:54 +0800

    接收网页请求

--- controller/controller.go
+++ controller/controller.go
@@ -9,3 +9,22 @@ func Exam(r *http.Request, w http.ResponseWriter) (res api.AppResponse) {
 
 	return res
 }
+func RecommendAppHomePage(r *http.Request, w http.ResponseWriter) (res api.AppResponse) {
+	defer func() {
+		if err := recover(); err != nil {
+			res.ResCode = api.APPRESPONSE_CODE_SUCCESS
+			res.ResStatus = api.REQUEST_ERR
+			Errorln(err)
+		}
+	}()
+
+	var param model.Param
+	userId := r.FormValue("userId")
+	param.SetUserId(userId)
+	uuid := r.FormValue("uuid")
+	param.SetUuid(uuid)
+	recAction := r.FormValue("action")
+	param.SetRecAction(recAction)
+	param.SetChannel(model.HOMECHANNEL)
+	res = recommend.RecommendEntry(&param)
+}
