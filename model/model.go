03f853a7d04bdd4b84bc90d4a0f611b74fc36594
zen <zhangyiming748@gmail.com>
2021-03-29 19:54:09 +0800

    定义推荐常量

--- model/model.go
+++ model/model.go
@@ -27,6 +27,34 @@ type Article struct {
 	RecommendTimeLimit int64              `protobuf:"varint,11,opt,name=recommendTimeLimit,proto3" json:"recommendTimeLimit,omitempty"`
 }
 
+const (
+	PERSONALALGO string = "personal"
+	RANDOMALGO   string = "random"
+
+	ACTION_UP   string = "up"
+	ACTION_DOWN string = "down"
+	ACTION_HOME string = "home"
+
+	RECOMMEND_SIZE_UP   = 10
+	RECOMMEND_SIZE_DOWN = 10
+	RECOMMEND_SIZE_HOME = 20
+
+	CATEGORY_RATIO = 0.8
+
+	SHOWLISTLEN      = 3000
+	CLICKLISTLEN     = 100
+	QUICKSHOWLISTLEN = 60
+
+	JSONP_CALLBACK_KEY string = "callback"
+	EXCEPTION_MSG      string = "系统错误"
+	DEVICE_TYPE_H5     string = "APP"
+	DEVICE_TYPE_PC     string = "PC"
+	DEVICE_PC          string = "PC"
+	DEVICE_H5          string = "H5"
+	DEVICE_APP         string = "APP"
+
+	ACTICLETAGSKEY = "articleTags"
+)
 func NewArticle(fields []byte) (article Article, err error) {
 	defer func() {
 		if e := recover(); e != nil {
